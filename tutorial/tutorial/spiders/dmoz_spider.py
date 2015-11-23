1e7f5f1087b66aa54f4d68166dc4c405f62ac6d3
Casey <fyd789@sohu.com>
2015-11-23 15:18:19 +0000

    normal

---
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -0,0 +1,22 @@
+from scrapy.spiders import Spider
+from scrapy.selector import Selector
+
+
+class DmozSpider(Spider):
+	name = "dmoz"
+	allowed_domains=["dmoz.org"]
+	start_urls=[
+		"http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",  
+		"http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+	]  
+
+
+def parse(self,response):
+	sel=Selector(response)
+	sites=sel.xpath('//ul/li')
+	for site in sites:
+		title=site.xpath('a/text()').extract()
+		ilnk=site.xpath('a/@href').extract()
+		desc=site.xpath('text()').extract()
+		print title
+	
